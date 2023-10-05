a60756e283fcf43fab577abc0e279f1a63080872
Gabriel Toth Gonçalves <60109701+GabrielToth@users.noreply.github.com>
2023-10-05 13:34:25 -0300

    FIX: Vídeos section
    FIX: Sessão de vídeos de acordo com o sistemasatfiscal.
    Responsividade já inclusa.
    
    UPDATE: Footer

--- src/app/components/videos/videos.component.ts
+++ src/app/components/videos/videos.component.ts
@@ -1,20 +1,49 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, AfterViewInit, Renderer2, ElementRef } from "@angular/core";
 
 @Component({
-  selector: "app-videos",
-  templateUrl: "videos.component.html",
-  styleUrls: ["videos.component.scss"],
+    selector: "app-videos",
+    templateUrl: "videos.component.html",
+    styleUrls: ["videos.component.scss"],
 })
-export class Videos {
-  @Input()
-  rootClassName: string = "";
-  @Input()
-  heading: string = "Soft Clever na Mídia";
-  @Input()
-  heading1: string = "Produtos e Serviços";
-  @Input()
-  text1: string = "Conheça nossos produtos e serviços em apenas 1 minuto";
-  @Input()
-  text: string = "Reportagem exibida na Band News";
-  constructor() {}
+export class Videos implements AfterViewInit {
+    constructor(private renderer: Renderer2, private el: ElementRef) { }
+
+    ngAfterViewInit() {
+        const youtubeElements = this.el.nativeElement.querySelectorAll('.youtube');
+
+        for (let i = 0; i < youtubeElements.length; i++) {
+            const element = youtubeElements[i] as HTMLElement;
+            const source = "https://img.youtube.com/vi/" + element.dataset.embed + "/sddefault.jpg";
+
+            let image = new Image();
+
+            if (element.dataset.embed === "S6Vouca8pwQ") {
+                image.src = '../assets/images/jpg/yt-thumb-pre-venda.jpg';
+            } else {
+                image.src = source;
+            }
+
+            // Adicione a classe "thumb-video" à imagem
+            image.classList.add('thumb-video');
+
+            image.addEventListener('load', function (i) {
+                return function () {
+                    youtubeElements[i].appendChild(image);
+                };
+            }(i));
+
+            element.addEventListener("click", function () {
+                let iframe = document.createElement("iframe");
+                iframe.setAttribute("frameborder", "0");
+                iframe.setAttribute("allowfullscreen", "");
+                iframe.setAttribute("src", "https://www.youtube.com/embed/" + this.dataset.embed + "?rel=0&showinfo=0&autoplay=1");
+
+                // Adicione a classe "youtube-video" ao iframe
+                iframe.classList.add('youtube-video');
+
+                this.innerHTML = "";
+                this.appendChild(iframe);
+            });
+        }
+    }
 }
--- src/app/pages/obrigado/obrigado.component.ts
+++ src/app/pages/obrigado/obrigado.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+declare const gtag: Function;
+
 @Component({
-  selector: 'app-obrigado',
-  templateUrl: './obrigado.component.html',
-  styleUrls: ['./obrigado.component.scss']
+    selector: 'app-obrigado',
+    templateUrl: './obrigado.component.html',
+    styleUrls: ['./obrigado.component.scss'],
 })
 export class ObrigadoComponent implements OnInit {
+    constructor() { }
 
-  constructor() { }
+    ngOnInit(): void {
+        // Configurar o Google Analytics (substitua 'GA_MEASUREMENT_ID' pelo seu ID de acompanhamento)
+        gtag('config', 'G-KDN1VFL478');
 
-  ngOnInit(): void {
-  }
-
+        // Disparar o evento de conversão
+        gtag('event', 'conversion', {'send_to': 'AW-1024274254/Nz-MCL-2xOgYEM7etOgD'});
+    }
 }
