d7412acc415ab664fc861980c581c49b584d1d04
Gabriel Toth Gonçalves <60109701+GabrielToth@users.noreply.github.com>
2023-10-05 17:58:22 -0300

    FIX: Leitura vertical
    Arrumado o fluxo de leitura das soluções.
    
    Pendente:
    Alteração no footer

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -12,13 +12,10 @@ import { LandingFiscalServicos } from "./landing-fiscal-servicos/landing-fiscal-
 import { LandingSATServicos } from "./landing-sat-servicos/landing-sat-servicos.component";
 import { LandingAtacadoServicos } from "./landing-atacado-servicos/landing-atacado-servicos.component";
 import { Footer } from "./footer/footer.component";
-import { CategoriaSolucaoAtacado } from "./categoria-solucao-atacado/categoria-solucao-atacado.component";
 import { NavigationLinks1 } from "./navigation-links1/navigation-links1.component";
 import { LogoIcone } from "./logo-icone/logo-icone.component";
 import { LandingServicoServicos } from "./landing-servico-servicos/landing-servico-servicos.component";
-import { CategoriaSolucaoVarejo } from "./categoria-solucao-varejo/categoria-solucao-varejo.component";
 import { NavigationLinks2 } from "./navigation-links2/navigation-links2.component";
-import { CategoriaSolucaoFiscal } from "./categoria-solucao-fiscal/categoria-solucao-fiscal.component";
 import { LandingFiscalHeroSection } from "./landing-fiscal-hero-section/landing-fiscal-hero-section.component";
 import { LandingAtacadoConteudo } from "./landing-atacado-conteudo/landing-atacado-conteudo.component";
 import { LandingSATHeroSection } from "./landing-sat-hero-section/landing-sat-hero-section.component";
@@ -26,8 +23,8 @@ import { CampoFORMS } from "./campo-forms/campo-forms.component";
 import { HomeServicos } from "./home-servicos/home-servicos.component";
 import { LandingFiscal } from "./landing-fiscal/landing-fiscal.component";
 import { Menu } from "./menu/menu.component";
-import { CategoriaSolucaoIndustria } from "./categoria-solucao-industria/categoria-solucao-industria.component";
 import { NavigationLinks } from "./navigation-links/navigation-links.component";
+import { SolutionCardComponent } from "./solution-card/solution-card.component";
 
 @NgModule({
   declarations: [
@@ -41,13 +38,10 @@ import { NavigationLinks } from "./navigation-links/navigation-links.component";
     LandingSATServicos,
     LandingAtacadoServicos,
     Footer,
-    CategoriaSolucaoAtacado,
     NavigationLinks1,
     LogoIcone,
     LandingServicoServicos,
-    CategoriaSolucaoVarejo,
     NavigationLinks2,
-    CategoriaSolucaoFiscal,
     LandingFiscalHeroSection,
     LandingAtacadoConteudo,
     LandingSATHeroSection,
@@ -55,8 +49,8 @@ import { NavigationLinks } from "./navigation-links/navigation-links.component";
     HomeServicos,
     LandingFiscal,
     Menu,
-    CategoriaSolucaoIndustria,
     NavigationLinks,
+    SolutionCardComponent
   ],
   imports: [CommonModule, RouterModule],
   exports: [
@@ -70,13 +64,10 @@ import { NavigationLinks } from "./navigation-links/navigation-links.component";
     LandingSATServicos,
     LandingAtacadoServicos,
     Footer,
-    CategoriaSolucaoAtacado,
     NavigationLinks1,
     LogoIcone,
     LandingServicoServicos,
-    CategoriaSolucaoVarejo,
     NavigationLinks2,
-    CategoriaSolucaoFiscal,
     LandingFiscalHeroSection,
     LandingAtacadoConteudo,
     LandingSATHeroSection,
@@ -84,8 +75,8 @@ import { NavigationLinks } from "./navigation-links/navigation-links.component";
     HomeServicos,
     LandingFiscal,
     Menu,
-    CategoriaSolucaoIndustria,
     NavigationLinks,
+    SolutionCardComponent
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
---
+++ src/app/components/solution-card/solution-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'app-solution-card',
+    templateUrl: './solution-card.component.html',
+    styleUrls: ['./solution-card.component.scss']
+})
+export class SolutionCardComponent {
+    @Input()
+    image_src?: string =
+        "/assets/images/atacado-e-distribuicao-1500w.webp";
+    @Input()
+    text?: string =
+        "NF-e, CT-e, NFS-e, Vendas, Liberação e Expedição de Pedidos, Reserva, Troca/Devolução, Financeiro, Cobrança, Compras e muito mais.";
+    @Input()
+    heading?: string = "Atacado e Distribuição";
+    @Input()
+    image_alt?: string = "Atacado e Distribuição";
+
+}
