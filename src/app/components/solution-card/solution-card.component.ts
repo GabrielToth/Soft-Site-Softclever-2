a48f950eae7683eb00b59453ddcbe63ee442db8a
Gabriel Toth Gonçalves <60109701+GabrielToth@users.noreply.github.com>
2023-10-17 17:45:00 -0300

    NEW VERSION 1.1.0 - BUG FIXES and Responsivity.
    BUG FIXES:
        Background preto da 2° hero section na home não preenchia toda a imagem.
        Os cards de solução não expandiam quando a quantidade era inferior a 4.
        Todas os componentes de soluções das landing pages está de acordo com a página modelo.
    
    ADD: Enumeração de versões lançadas, funcionamento da enumeração na documentação

--- src/app/components/solution-card/solution-card.component.ts
+++ src/app/components/solution-card/solution-card.component.ts
@@ -6,13 +6,8 @@ import { Component, Input } from '@angular/core';
     styleUrls: ['./solution-card.component.scss'],
 })
 export class SolutionCardComponent {
-    @Input()
-    image?: string = '/assets/images/atacado-e-distribuicao-1500w.webp';
-    @Input()
-    text?: string =
-        'NF-e, CT-e, NFS-e, Vendas, Liberação e Expedição de Pedidos, Reserva, Troca/Devolução, Financeiro, Cobrança, Compras e muito mais.';
-    @Input()
-    heading?: string = 'Atacado e Distribuição';
-    @Input()
-    image_alt?: string = 'Atacado e Distribuição';
+    @Input() heading!: string
+    @Input() text!: string
+    @Input() image!: string
+    @Input() image_alt?: string
 }
